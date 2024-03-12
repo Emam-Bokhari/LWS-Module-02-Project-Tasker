9047182f922fa9c789d53cdf4ea6fc1e2561ced8
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2024-03-12 12:17:54 +0600

    create TaskBoard component with tasklist,search box

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { Fragment } from "react";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
+import TaskBoard from "./components/TaskBoard/TaskBoard";
 
 export default function App() {
   return (
     <Fragment>
-      
+
       <Navbar />
       <HeroSection />
+      <TaskBoard />
+
       
     </Fragment>
   )
