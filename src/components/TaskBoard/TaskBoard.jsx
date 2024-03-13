94ed820f5427a2fcfa963b3d0bf5938ed88e10c9
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2024-03-13 11:21:20 +0600

    Add default task and TaskBoard component - Add default task to TaskBoard for initial setup - Implement TaskList component to display tasks in a table format

--- src/components/TaskBoard/TaskBoard.jsx
+++ src/components/TaskBoard/TaskBoard.jsx
@@ -1,20 +1,33 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import Searchbox from "./Searchbox";
 import TaskActions from "./TaskActions";
 import TaskList from "./TaskList";
 
 export default function TaskBoard() {
+
+    const defaultTask = {
+        "id": crypto.randomUUID(),
+        "title": "Integration API",
+        "description": "Connect an existing API to a third-party database using secure methods and handle data exchange efficiently.",
+        "tags": ["Web", "Python", "Api"],
+        "priority": "High",
+        "isFavourite": true
+    }
+
+    const [tasks, setTasks] = useState([defaultTask])
+
     return (
         <Fragment>
             <section className="mb-20" id="tasks">
 
                 <div className="container">
-                    {/* <!-- Search Box --> */}
+                    {/* Search Box */}
                     <Searchbox />
-                    {/* <!-- Search Box Ends --> */}
+
                     <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
                         <div className="mb-14 items-center justify-between sm:flex">
                             <h2 className="text-2xl font-semibold max-sm:mb-4">Your Tasks</h2>
+                            {/* Task actions */}
                             <TaskActions />
                         </div>
                         <div className="overflow-auto">
@@ -30,8 +43,8 @@ export default function TaskBoard() {
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    
-                                    <TaskList />
+                                    {/* Task list */}
+                                    <TaskList tasks={tasks} />
 
                                 </tbody>
                             </table>
